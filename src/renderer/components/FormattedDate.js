7e5e10bdd85e9a6a4b5b068415ff676f9e662630
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-10-13 18:00:03 +0200

    Add link to moment.js doc about formatting in FormattedDate
    because the moment.js doc is always horrible to navigate

--- src/renderer/components/FormattedDate.js
+++ src/renderer/components/FormattedDate.js
@@ -9,6 +9,8 @@ type Props = {
 
 /* TODO: Replace this function with the useDateTimeFormat hook */
 function FormattedDate({ date, format = "L LT" }: Props) {
+  // For the format, prefer formats that are purely digital
+  // https://momentjs.com/docs/#/parsing/string-format/#
   return moment(date).format(format);
 }
 
