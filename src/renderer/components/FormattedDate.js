db488b4940ac1515be49705173078313cc907003
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2021-10-13 18:00:53 +0200

    Fix moment.js link

--- src/renderer/components/FormattedDate.js
+++ src/renderer/components/FormattedDate.js
@@ -10,7 +10,7 @@ type Props = {
 /* TODO: Replace this function with the useDateTimeFormat hook */
 function FormattedDate({ date, format = "L LT" }: Props) {
   // For the format, prefer formats that are purely digital
-  // https://momentjs.com/docs/#/parsing/string-format/#
+  // https://momentjs.com/docs/#/parsing/string-format/
   return moment(date).format(format);
 }
 
